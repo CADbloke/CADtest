bebfe94859a5379cc8b547fe6955731de7a6ad88
Ewen @ 8560wX <Ewen@CADbloke.com>
2016-03-07 20:09:10 +1100

    Resharpered my stupid indents of 2 into 4, like everybody else.
    I'm glad that stupid obsession is over. oh, wait, all my repos! oh noooooo

--- CADtest-CS/CADtestApp.cs
+++ CADtest-CS/CADtestApp.cs
@@ -4,17 +4,18 @@ using System;
 using System.Runtime.InteropServices;
 using Autodesk.AutoCAD.ApplicationServices.Core;
 using Autodesk.AutoCAD.Runtime;
+using CADTestRunner;
 
-[assembly: ExtensionApplication(typeof (CADTestRunner.NUnitRunnerApp))]
+[assembly: ExtensionApplication(typeof (NUnitRunnerApp))]
 
 namespace CADTestRunner
 {
-  /// <summary> AutoCAD extension application implementation. </summary>
-  /// <seealso cref="T:Autodesk.AutoCAD.Runtime.IExtensionApplication"/>
-  public class NUnitRunnerApp : IExtensionApplication
-  {
-    public void Initialize()
+    /// <summary> AutoCAD extension application implementation. </summary>
+    /// <seealso cref="T:Autodesk.AutoCAD.Runtime.IExtensionApplication" />
+    public class NUnitRunnerApp : IExtensionApplication
     {
+        public void Initialize()
+        {
 #if !CoreConsole
       if ( !AttachConsole(-1) )  // Attach to a parent process console
       AllocConsole(); // Alloc a new console if none available
@@ -22,58 +23,57 @@ namespace CADTestRunner
 #else
 // http://forums.autodesk.com/t5/net/accoreconsole-exe-in-2015-doesn-t-do-system-console-writeline/m-p/5551652
 // This code is so you can see the test results in the console window, as per the above forum thread.
-      if (Application.Version.Major * 10 + Application.Version.Minor > 190) // >v2013
-      {
-        // http://stackoverflow.com/a/15960495/492
-        // stdout's handle seems to always be equal to 7
-        IntPtr defaultStdout = new IntPtr(7);
-        IntPtr currentStdout = GetStdHandle(StdOutputHandle);
-        if (currentStdout != defaultStdout) { SetStdHandle(StdOutputHandle, defaultStdout); }
-        
+            if (Application.Version.Major * 10 + Application.Version.Minor > 190) // >v2013
+            {
+                // http://stackoverflow.com/a/15960495/492
+                // stdout's handle seems to always be equal to 7
+                var defaultStdout = new IntPtr(7);
+                IntPtr currentStdout = GetStdHandle(StdOutputHandle);
+                if (currentStdout != defaultStdout)
+                    SetStdHandle(StdOutputHandle, defaultStdout);
+
 // http://forums.autodesk.com/t5/net/accoreconsole-exe-in-2015-doesn-t-do-system-console-writeline/m-p/5551652#M43708
-        FixStdOut();
-      }
+                FixStdOut();
+            }
 #endif
-    }
+        }
 
-
-    public void Terminate()
-    {
+        public void Terminate()
+        {
 #if !CoreConsole
       FreeConsole();
 #endif
-    }
-
-
+        }
 
 // http://stackoverflow.com/questions/3917202/how-do-i-include-a-console-in-winforms/3917353#3917353
-    /// <summary> Allocates a new console for current process. </summary>
-    [DllImport("kernel32.dll")]
-    public static extern Boolean AllocConsole();
+        /// <summary> Allocates a new console for current process. </summary>
+        [DllImport("kernel32.dll")]
+        public static extern bool AllocConsole();
 
-    /// <summary> Frees the console. </summary>
-    [DllImport("kernel32.dll")]
-    public static extern Boolean FreeConsole();
+        /// <summary> Frees the console. </summary>
+        [DllImport("kernel32.dll")]
+        public static extern bool FreeConsole();
 
-    // http://stackoverflow.com/a/8048269/492
-    [DllImport("kernel32.dll")]
-    private static extern bool AttachConsole(int pid);
-
+        // http://stackoverflow.com/a/8048269/492
+        [DllImport("kernel32.dll")]
+        private static extern bool AttachConsole(int pid);
 
 #if CoreConsole
 // http://forums.autodesk.com/t5/net/accoreconsole-exe-in-2015-doesn-t-do-system-console-writeline/m-p/5551652#M43708
 // trying to fix the telex-like line output from Core Console in versions > 2013. This didn't work for me. :(
-    [DllImport("msvcr110.dll")]
-    private static extern int _setmode(int fileno, int mode);
-    public void FixStdOut() { _setmode(3, 0x4000); }
+        [DllImport("msvcr110.dll")]
+        private static extern int _setmode(int fileno, int mode);
 
+        public void FixStdOut() { _setmode(3, 0x4000); }
 
 // http://stackoverflow.com/a/15960495/492
-    private const UInt32 StdOutputHandle = 0xFFFFFFF5;
-    [DllImport("kernel32.dll")]
-    private static extern IntPtr GetStdHandle(UInt32 nStdHandle);
-    [DllImport("kernel32.dll")]
-    private static extern void SetStdHandle(UInt32 nStdHandle, IntPtr handle);
+        private const uint StdOutputHandle = 0xFFFFFFF5;
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetStdHandle(uint nStdHandle);
+
+        [DllImport("kernel32.dll")]
+        private static extern void SetStdHandle(uint nStdHandle, IntPtr handle);
 #endif
-  }
+    }
 }
--- CADtest-CS/CADtestRunner.cs
+++ CADtest-CS/CADtestRunner.cs
@@ -3,35 +3,34 @@
 #if CoreConsole
 #endif
 using System;
-using Autodesk.AutoCAD.Runtime;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
+using Autodesk.AutoCAD.Runtime;
 using CADTestRunner;
+using NUnitLite;
 
 [assembly: CommandClass(typeof (NUnitLiteTestRunner))]
+
 namespace CADTestRunner
 {
-  public class NUnitLiteTestRunner
-  {
-     /// <summary> This command runs the NUnit tests in this assembly. </summary>
-    [CommandMethod("RunCADtests", CommandFlags.Session)]
-    public void RunCADtests()
-     {
-       string directoryName = Path.GetTempPath();//Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-       string[] nunitArgs = new List<string>
-       {
-         // for details of options see  http://www.nunit.com/index.php?p=nunitliteOptions&r=3.0
-         "--verbose"                 // Tell me everything
-         // , "--work=" + directoryName // save TestResults.xml to the build folder
-         , "--wait"                  // Wait for input before closing console window (PAUSE). Comment this out for batch operations.
-       }.ToArray();
-      
-      
-       new NUnitLite.AutoRun().Execute(nunitArgs); 
-      // https://github.com/nunit/nunit/commit/6331e7e694439f8cbf000156f138a3e10370ec40
+    public class NUnitLiteTestRunner
+    {
+        /// <summary> This command runs the NUnit tests in this assembly. </summary>
+        [CommandMethod("RunCADtests", CommandFlags.Session)]
+        public void RunCADtests()
+        {
+            string directoryName = Path.GetTempPath(); //Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string[] nunitArgs = new List<string>
+                {
+                    // for details of options see  http://www.nunit.com/index.php?p=nunitliteOptions&r=3.0
+                    "--verbose" // Tell me everything
+                    // , "--work=" + directoryName // save TestResults.xml to the build folder
+                    // , "--wait" // Wait for input before closing console window (PAUSE). Comment this out for batch operations.
+                }.ToArray();
 
-       Console.ReadLine();
-     }
-  }
+
+            new AutoRun().Execute(nunitArgs);
+            // https://github.com/nunit/nunit/commit/6331e7e694439f8cbf000156f138a3e10370ec40
+        }
+    }
 }
